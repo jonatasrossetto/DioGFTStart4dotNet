79075988595dadef2bc38752ff9648845d6a8e09
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2022-03-01 11:27:22 -0300

    adicionado exemplo streamreader e arquivos csv

---
+++ CSVStream/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+class program
+{
+    static void Main()
+    {
+        var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "usuarios_exportacao.csv");
+        using var sr = new StreamReader(path);
+        var cabecalho = sr.ReadLine()?.Split(',');
+
+        // foreach (string item in cabecalho) WriteLine(item);
+
+        while (true)
+        {
+            var registro = sr.ReadLine()?.Split(',');
+            if (registro == null) break;
+            for (int i = 0; i < registro.Length; i++)
+            {
+                WriteLine($"{cabecalho[i]}: {registro[i]}");
+            }
+            WriteLine("-------------------");
+        }
+
+
+        WriteLine("console");
+    }
+
+}
